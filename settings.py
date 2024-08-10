1cc3dbf0e48b09fcadae1d0eb2d42d0e013de281
roviedoinnovasof <roviedo@innovasof.com>
2024-08-10 14:49:43 -0400

    update chatflow id

--- settings.py
+++ settings.py
@@ -9,7 +9,7 @@ class Settings(BaseSettings):
     layout: str = "centered"
 
     flowise_base_url: str = "https://reinaldoviedo94-flowise.hf.space/"
-    flowise_chatflow_id: str = "267ea9e5-45f4-479c-8261-39ba8cc9c019"
+    flowise_chatflow_id: str = "6c021f22-11b1-4dcf-b3a6-61e6c326fe26"
     flowise_username: str = "c42ed0ed18dc07cad699a742a4a7cc7c"
     flowise_password: str = "ad1602569190bef23d448b7c2453239d"
 
